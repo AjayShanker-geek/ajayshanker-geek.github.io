767d32241d903a0a6a054dc9a76c52eae81b3625
Ajay Shanker <ajayshanker1998@gmail.com>
2020-12-24 21:42:12 +0800

    Version 0.1
    Website for owadio App

---
+++ assets/js/popup.js
@@ -0,0 +1,9 @@
+
+// When the user clicks on <div>, open the popup
+function togglePopupOne(){
+    document.getElementById("popup-1").classList.toggle("active");
+  }
+
+function togglePopupTwo(){
+    document.getElementById("popup-2").classList.toggle("active");
+  }
